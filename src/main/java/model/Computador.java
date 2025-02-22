cfcf47332c7027119505b1ab3f58f4b3a8735ca1
Emagmunioz <emagmunioz@gmail.com>
2025-02-22 23:17:11 +0100

    generate getterTest not run

--- src/main/java/model/Computador.java
+++ src/main/java/model/Computador.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Computador {
+    private String marca;
+    private int memoria;
+    private String procesador;
+    private String sistemaOp;
+    private double precio;
+
+    public Computador(String marca, int memoria, String procesador, String sistemaOp, double precio) 
+
+
+
+
+
 
 }
--- src/test/java/model/test/Computador/test.java
+++ src/test/java/model/test/Computador/test.java
@@ -1,5 +1,25 @@
 package model.test.Computador;
+import org.junit.jupiter.api.Test;
 
+import model.Computador;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class test {
-
+    @Test
+    public void testCrearComputador() {
+        
+        String marca = "Dell";
+        int memoria = 16;
+        String procesador = "Intel i7";
+        String sistemaOp = "Windows 10";
+        double precio = 1500.0;
+        Computador computador = new Computador(marca, memoria, procesador, sistemaOp, precio); 
+        assertThat(computador.getMarca(), is(marca));
+        assertThat(computador.getMemoria(), is(memoria));
+        assertThat(computador.getProcesador(), is(procesador));
+        assertThat(computador.getSistemaOp(), is(sistemaOp));
+        assertThat(computador.getPrecio(), is(precio));
+    } 
 }
+

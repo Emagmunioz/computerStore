6407e01d62c76245b650bf5e7c1dcc1d1ed320a3
Emagmunioz <emagmunioz@gmail.com>
2025-02-22 23:55:51 +0100

    testAgregarComputador not run

--- src/test/java/service/test/TiendaServices/test.java
+++ src/test/java/service/test/TiendaServices/test.java
@@ -1,5 +1,21 @@
 package service.test.TiendaServices;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import java.util.List;
+import model.Tienda;
+import model.Computador;
 
 public class test {
+    private Tienda tienda;
+    private TiendaService tiendaService;
+
+    @BeforeEach
+    public void setUp() {
+        tienda = new Tienda("TechStore", "Juan Perez", "123456789");
+        tiendaService = new TiendaService(tienda);
+    }
+
 
 }

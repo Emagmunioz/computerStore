028ce4179e170b2cba682e0171e7b5d93ea0a784
Emagmunioz <emagmunioz@gmail.com>
2025-02-22 23:27:07 +0100

    create Tienda test not run

--- src/test/java/model/test/Computador/Tienda/test.java
+++ src/test/java/model/test/Computador/Tienda/test.java
@@ -1,5 +1,8 @@
 package model.test.Computador.Tienda;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class test {
 
 }

4ff345549927c383b5e2f454196daf413e43acb9
Emagmunioz <emagmunioz@gmail.com>
2025-02-22 23:35:25 +0100

    testCrearTienda not run

--- src/main/java/model/Tienda.java
+++ src/main/java/model/Tienda.java
@@ -1,5 +1,17 @@
 package model;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Tienda {
+    private String nombre;
+    private String propietario;
+    private String idTributario;
+    private List<Computador> computadores;
+
+    public Tienda(String nombre, String propietario, String idTributario) {
+        this.nombre = nombre;
+        this.propietario = propietario;
+        this.idTributario = idTributario;
+        this.computadores = new ArrayList<>();
+    }  
 
 }
--- src/test/java/model/test/Computador/Tienda/test.java
+++ src/test/java/model/test/Computador/Tienda/test.java
@@ -1,8 +1,23 @@
 package model.test.Computador.Tienda;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import model.Tienda;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 public class test {
     private Tienda tienda;
+    @BeforeEach
+    public void setUp() {
+        tienda = new Tienda("TechStore", "Juan Perez", "123456789");
+    }
+    @Test
+    public void testCrearTienda() {
+        // Assert
+        assertThat(tienda.getNombre(), is("TechStore"));
+        assertThat(tienda.getPropietario(), is("Juan Perez"));
+        assertThat(tienda.getIdTributario(), is("123456789"));
+        assertThat(tienda.getComputadores(), is(empty()));
+    }
 }

93c562035ce8b34211b0a1c70a9bc2e2b42a2953
Emagmunioz <emagmunioz@gmail.com>
2025-02-22 23:22:08 +0100

    test computer run ok

--- src/main/java/model/Computador.java
+++ src/main/java/model/Computador.java
@@ -9,8 +9,18 @@ public class Computador {
 
     public Computador(String marca, int memoria, String procesador, String sistemaOp, double precio) 
 
-
-
+    {
+        this.marca = marca;
+        this.memoria = memoria;
+        this.procesador = procesador;
+        this.sistemaOp = sistemaOp;
+        this.precio = precio;
+    }
+    public String getMarca() { return marca; }
+    public int getMemoria() { return memoria; }
+    public String getProcesador() { return procesador; }
+    public String getSistemaOp() { return sistemaOp; }
+    public double getPrecio() { return precio; }
 
 
 
